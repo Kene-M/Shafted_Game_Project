ac43ed4dd5b1314c6b6a00e09ef6011e283f2322
qmcco <qmccollister03@gmail.com>
2026-02-05 12:34:09 -0600

    (2/5/26) - Added Dash Functionality To Movement
    Added dash mechanic to movement. Upon press of the spacebar, the player
    character will increase speed to 3000 for 10 ticks. There is a 60 tick
    delay before a dash can be performed again.

--- shafted-godot-project/projectile.gd
+++ shafted-godot-project/projectile.gd
@@ -3,11 +3,5 @@ extends AnimatableBody2D
 @export var direction = Vector2(0,0)
 @onready var sprite = $Sprite2D
 
-#func _ready():
-	#var img = Image.load_from_file("res://Brick2-1.jpg")
-	#var texture = ImageTexture.create_from_image(img)
-	#sprite.texture = texture
-	#sprite.scale = Vector2(0.01, 0.01)
-	
 func _physics_process(delta: float) -> void:
 	position += (direction * speed) * delta
