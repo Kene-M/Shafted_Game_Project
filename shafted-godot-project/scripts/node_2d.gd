81339668c9eb35669a7ec661b1cacece87043eb3
qmcco <qmccollister03@gmail.com>
2026-02-09 14:44:09 -0600

    (2/9/2026) - Temp change to file structure
    Changed organization of files

--- shafted-godot-project/scripts/node_2d.gd
+++ shafted-godot-project/scripts/node_2d.gd
@@ -1,14 +1,18 @@
 extends Node2D
 @onready var label = $Label
 @onready var label2 = $Label2
-@onready var char = $CharacterBody2D
 @export var proj_speed = 1000
 
 func _ready():
-	char.connect("update_speed", _on_update_speed)
-	char.connect("fire_projectile", _on_fire_projectile)
+	var char = preload("res://scenes/character_body_2d.tscn")
+	var char_inst = char.instantiate()
+	char_inst.global_position = Vector2(0,0)
+	char_inst.name = "PlayerChar"
+	add_child(char_inst)
+	char_inst.connect("update_speed", _on_update_speed)
+	char_inst.connect("fire_projectile", _on_fire_projectile)
 	label.text = "0"
-	var projectile = preload("res://projectile.tscn")
+	var projectile = preload("res://scenes/projectile.tscn")
 	var proj_inst = projectile.instantiate()
 	proj_inst.global_position = Vector2(0,0)
 	add_child(proj_inst)
@@ -20,8 +24,9 @@ func _on_update_speed(speed) -> void:
 	
 func _on_fire_projectile(direction) -> void:
 	label2.text = "_on_fire_projectile called"
-	var projectile = preload("res://projectile.tscn")
+	var projectile = preload("res://scenes/projectile.tscn")
 	var proj_inst = projectile.instantiate()
+	var char = get_node("PlayerChar")
 	proj_inst.global_position = char.position
 	add_child(proj_inst)
 	proj_inst.direction = direction
