5a656650d7d7b0abe919cb8eb6c25df6045e7d95
qmcco <qmccollister03@gmail.com>
2026-02-10 14:49:18 -0600

    Conflict resolution

--- shafted-godot-project/scripts/character_body_2d.gd
+++ shafted-godot-project/scripts/character_body_2d.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
-@export var speed = 300
+@export var speed = 0
+@export var min_speed = 0
+@export var max_speed = 0
+@export var dash_speed = 0
 @export var cur_direction = Vector2(0,0)
 @export var dash_ticks = 0
 @export var pre_dash_speed = 0
@@ -11,23 +14,23 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	if (direction != Vector2(0,0)):
 		cur_direction = direction
-		if (speed < 700):
+		if (speed < max_speed):
 			speed += 80
 		if (dash_ticks != 0):
 			dash_ticks -= 1
 			if dash_ticks == 60:
 				speed = pre_dash_speed
 			elif dash_ticks > 60:
-				speed = 3000
+				speed = dash_speed
 		elif Input.is_action_just_pressed("dash"):
 			dash_ticks = 70
 			pre_dash_speed = speed
-			speed = 3000
+			speed = dash_speed
 		velocity = direction * speed
-	if (direction == Vector2(0,0)) and (speed > 300):
+	if (direction == Vector2(0,0)) and (speed > min_speed):
 		speed -= 80
 		velocity = cur_direction * speed
-	elif (direction == Vector2(0,0)) and (speed <= 300):
+	elif (direction == Vector2(0,0)) and (speed <= min_speed):
 		velocity = Vector2(0,0)
 	
 		
--- shafted-godot-project/scripts/node_2d.gd
+++ shafted-godot-project/scripts/node_2d.gd
@@ -1,6 +1,4 @@
 extends Node2D
-@onready var label = $Label
-@onready var label2 = $Label2
 @export var proj_speed = 1000
 
 func _ready():
@@ -8,10 +6,14 @@ func _ready():
 	var char_inst = char.instantiate()
 	char_inst.global_position = Vector2(0,0)
 	char_inst.name = "PlayerChar"
+	char_inst.min_speed = 50
+	char_inst.max_speed = 300
+	char_inst.dash_speed = 1000
+	char_inst.scale = Vector2(0.4,0.4)
 	add_child(char_inst)
 	char_inst.connect("update_speed", _on_update_speed)
 	char_inst.connect("fire_projectile", _on_fire_projectile)
-	label.text = "0"
+
 	var projectile = preload("res://scenes/projectile.tscn")
 	var proj_inst = projectile.instantiate()
 	proj_inst.global_position = Vector2(0,0)
@@ -20,10 +22,9 @@ func _ready():
 	
 
 func _on_update_speed(speed) -> void:
-	label.text = str(speed)
+	pass
 	
 func _on_fire_projectile(direction) -> void:
-	label2.text = "_on_fire_projectile called"
 	var projectile = preload("res://scenes/projectile.tscn")
 	var proj_inst = projectile.instantiate()
 	var char = get_node("PlayerChar")
