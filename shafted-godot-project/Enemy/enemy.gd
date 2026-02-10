2a38196ae6d33febd7b14c7f6cb8187ff1d88296
Dhruv Bhakta <112499960+Dhruv-145@users.noreply.github.com>
2026-02-10 14:52:12 -0600

    enemy ai refactor

--- shafted-godot-project/Enemy/enemy.gd
+++ shafted-godot-project/Enemy/enemy.gd
@@ -1,3 +1,4 @@
+class_name Enemy
 extends CharacterBody2D
 
 
@@ -10,14 +11,16 @@ var home_pos = Vector2.ZERO
 func _ready():
 	home_pos = self.global_position
 	
-	nav_agent.path_desired_distance =4
+	nav_agent.path_desired_distance = 4
 	nav_agent.target_desired_distance = 4
-	
-func _physics_process(_detla):
+
+func _physics_process(_delta):
 	if nav_agent.is_navigation_finished():
 		return
+	
 	var axis = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = axis * speed
+	
 	move_and_slide()
 
 func recalc_path():
@@ -25,14 +28,14 @@ func recalc_path():
 		nav_agent.target_position = target_node.global_position
 	else:
 		nav_agent.target_position = home_pos
-	
 
-
-func _on_recalculate_timer_timeout() -> void:
+func _on_recalculate_timer_timeout():
 	recalc_path()
 
-func _on_aggro_range_area_entered(area: Area2D) -> void:
+func _on_aggro_range_area_entered(area):
 	target_node = area.owner
+	
+
 
 func _on_de_aggro_range_area_exited(area: Area2D) -> void:
 	if area.owner == target_node:
