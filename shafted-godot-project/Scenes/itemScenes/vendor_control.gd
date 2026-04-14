5f36bb9614795be4cb56836b0b1123b8c9bf5d0a
qmcco <qmccollister03@gmail.com>
2026-04-14 13:36:27 -0500

    (4/14/2026) - Added Tree node to augment vendor to replace item list logic (first iteration)
    Added early iteration of Tree node to replace item lists used in augment vendor. This is being done for more UI element flexibility.

--- shafted-godot-project/Scenes/itemScenes/vendor_control.gd
+++ shafted-godot-project/Scenes/itemScenes/vendor_control.gd
@@ -10,12 +10,19 @@ func _create_augments():
 			["HealthUp", rng.randi_range(50,200), AugType.Type.HPADD],
 			["SpeedUp", rng.randi_range(15,45), AugType.Type.SPDADD]
 		]
+	var tree = $TabContainer/Tree
+	var root = tree.create_item()
+	tree.hide_root = true
 	while len(aug_args) != 0:
+		var new_child = tree.create_item(root)
 		var aug = aug_args.pick_random()
 		var new_aug = Augment.new()
 		new_aug.aug_name = aug[0]
 		new_aug.data = aug[1]
 		new_aug.type = aug[2]
+		new_child.set_text(0, new_aug.aug_name)
+		for i in range(1,6):
+			new_child.set_text(i, str(i))
 		var index = buy_list.add_item(new_aug.aug_name)
 		buy_list.set_item_metadata(index, new_aug)
 		aug_args.erase(aug)
@@ -30,7 +37,10 @@ func _on_static_augment_vendor_2d_open_shop() -> void:
 		sell_list.set_item_metadata(index, i)
 	if buy_list_set == false:
 		_create_augments()
+	
+	
 	get_tree().paused = true
+	
 	
 
 
@@ -38,3 +48,10 @@ func _on_button_pressed() -> void:
 	self.visible = false
 	sell_list.clear()
 	get_tree().paused = false
+
+
+func _on_tree_cell_selected() -> void:
+	var tree = $TabContainer/Tree
+	var sel_item = tree.get_selected()
+	if sel_item:
+		print(sel_item)
