b8a7aace37215f8cc001b0a08bfb59325f00a5f7
qmcco <qmccollister03@gmail.com>
2026-04-19 23:01:10 -0500

    (4/19/2026) - Added Hands to Launcher

--- shafted-godot-project/Scripts/weaponScripts/weaponLogicScripts/launcher.gd
+++ shafted-godot-project/Scripts/weaponScripts/weaponLogicScripts/launcher.gd
@@ -17,11 +17,11 @@ func init():
 	var char = get_parent()
 	char.cur_run = "no_arms_run"
 	sprite.texture = texture
-	sprite.scale = Vector2(3.5, 3.5)
+	sprite.scale = Vector2(3.7, 3.7)
 	if $"../AnimatedSprite2D".flip_h == false:
-		sprite.position = Vector2(7, 13)
+		sprite.position = Vector2(4, 13)
 	else:
-		sprite.position = Vector2(-7, 13)
+		sprite.position = Vector2(-4, 13)
 	sprite.rotation = 0
 
 func _on_fire_projectile(direction, augment_vals) -> void:
